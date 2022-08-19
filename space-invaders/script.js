b118c4b9c33751371639f8f8bcef22f72995b978
lukedwards99 <70722474+lukedwards99@users.noreply.github.com>
2022-08-18 22:29:08 -0500

    first commit

---
+++ space-invaders/script.js
@@ -0,0 +1,21 @@
+
+const grid = document.getElementById("grid");
+
+let squareRed = false;
+
+for(let i = 0; i < 12; i++){
+
+    const newRow = document.createElement('div');
+    newRow.className = "row";
+    newRow.setAttribute("data-rowindex", i); 
+    grid.appendChild(newRow);
+
+    for(let j = 0; j < 12; j++){
+        const newSquare = document.createElement('div');
+        newSquare.className = "col" + (squareRed ? "red" : "");
+        squareRed = !squareRed;
+        newSquare.setAttribute("data-colindex", j);
+        //newSquare.innerHTML = "O";
+        newRow.appendChild(newSquare);    
+    }
+}
