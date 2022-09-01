6089e1ac88735948c6d8851c19fa26e36018044c
lukedwards99 <70722474+lukedwards99@users.noreply.github.com>
2022-08-31 23:02:08 -0500

    added ability to shoot projectiles

--- astroids/game.js
+++ astroids/game.js
@@ -4,13 +4,16 @@
 //const HEIGHT = 600;
 const FPS = 30;
 const MAX_SPEED = 5; //max speed of player (pixels per frame)
+const TURN_SPEED = 3;
+const PROJ_SPEED = 10; //speed of projectiles (px/frame)
 
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
 let upPressed = false;
 
-const GameObjects = new Set();
+//const GameObjects = new Set();
+const Projectiles = new Set();
 
 let player = { //player object
     x: canvas.width / 2,
@@ -22,8 +25,7 @@ let player = { //player object
     angle: 0
 }
 
-function UpdatePhysics() {
-
+function UpdatePlayerPos(){
     if(upPressed == true) {
         if(player.speed < MAX_SPEED){
             player.speed++;
@@ -37,28 +39,50 @@ function UpdatePhysics() {
     player.angle += player.moveAngle * Math.PI / 180;
     player.x += player.speed * Math.sin(player.angle);
     player.y -= player.speed * Math.cos(player.angle);
+}
 
+function UpdatePhysics() {
+
+    UpdatePlayerPos();
+    UpdateProjectilePos();
+}
+
+function UpdateProjectilePos(){
+    Projectiles.forEach(function (proj) {
+        proj.x += PROJ_SPEED * Math.sin(proj.angle);
+        proj.y -= PROJ_SPEED * Math.cos(proj.angle);
+    });
+}
+
+function DrawProjectiles(){
+    Projectiles.forEach(function (proj) {
+        ctx.save();
+        ctx.translate(proj.x, proj.y);
+        ctx.rotate(proj.angle);
+        ctx.beginPath();
+        ctx.moveTo(0, 0);
+        ctx.lineTo(10, 20);
+        ctx.lineTo( -10, 20);
+        ctx.lineTo(0, 0);
+        ctx.strokeStyle = "#000000";
+        ctx.stroke();
+        ctx.restore();  
+    });
 }
 
 function Draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height); //clear canvas to redraw
 
     DrawPlayer();
+    DrawProjectiles();
 
 }
 
 function DrawPlayer() {
-    
-
     ctx.save();
     ctx.translate(player.x, player.y);
     ctx.rotate(player.angle);
-    // ctx.fillStyle = "red";
-    // ctx.fillRect(50 / -2, 50 / -2, 50, 50);
-
     ctx.beginPath();
-    //ctx.arc(player.x, player.y, 40, 0, 2 * Math.PI);
-
     ctx.moveTo(0, 0);
     ctx.lineTo(10, 20);
     ctx.lineTo( -10, 20);
@@ -73,10 +97,10 @@ function InitEvents() {
     document.addEventListener('keydown', (event) => {
 
         switch(event.code){
-            case "ArrowLeft": player.moveAngle = -3; break;
-            case "ArrowRight": player.moveAngle = 3; break;
+            case "ArrowLeft": player.moveAngle = -TURN_SPEED; break;
+            case "ArrowRight": player.moveAngle = TURN_SPEED; break;
             case "ArrowUp": upPressed = true; break;
-            //case "ArrowDown": ArrowDownEventHandler(); break;
+            case "Space": Fire(); break;
             //default: alert(event.code);
         }
     });
@@ -90,6 +114,17 @@ function InitEvents() {
             //default: alert(event.code);
         }
     });
+}
+
+function Fire(){
+
+    let projectile = {
+        x: player.x,
+        y: player.y,
+        angle: player.angle
+    }
+
+    Projectiles.add(projectile);
 }
 
 
