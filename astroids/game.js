dea9008bfd7eff72c93e8a63add47eeeea2412f7
lukedwards99 <70722474+lukedwards99@users.noreply.github.com>
2022-09-04 21:35:03 -0500

    some last gameplay improvements

--- astroids/game.js
+++ astroids/game.js
@@ -1,17 +1,21 @@
 "use strict";
 
-const FPS = 30;
+const FPS = 60;
 const MAX_SPEED = 5; //max speed of player (pixels per frame)
-const PLAYER_ACCELERATION = .5 //acceleration of player
+const PLAYER_ACCELERATION = .25 //acceleration of player
 const TURN_SPEED = 5;
 const PROJ_SPEED = 20; //speed of projectiles (px/frame)
+const PROJ_LENGTH = 40; //shouldnt be less that PROJ_SPEED.
 const MIN_ASTROID_SPEED = 2;
-const MAX_ASTROID_SPEED = 10;
+const SPEED_PUNSHIMENT = 2; //if player is standing still min astroid speed goes up (shouldnt be larger than MIN_ASTROID_RADIUS - MIN_ASTROID_SPEED)
+const MAX_ASTROID_SPEED = 7;
 const MIN_ASTROID_RADIUS = 10;
+const RADIUS_PUNISHMENT = 5; //if player is standing still min astroid radius goes down (shouldnt be less than MIN_ASTROID_RADIUS)
 const MAX_ASTROID_RADIUS = 40;
 const PLAYER_RADIUS = 10;
 const MIN_ASTROID_SPAWN_COOLDOWN = 10; //in frames
 const PLAYER_SAFE_SPAWN_RADIUS = 200; //radius around player where astroids cant spawn
+const OUT_OF_BOUNDS_BUFFER = 200; //how many pixels out of bounds before game over or deletion of object
 
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
@@ -41,6 +45,12 @@ let player = { //player object
 }
 
 function UpdatePlayerPos(){
+    if(InBounds(player) == false){
+        gameOver == true;
+        GameOver();
+        return;
+    }
+
     if(upPressed == true && downPressed != true) {
         if(player.speed < MAX_SPEED){
             player.speed += PLAYER_ACCELERATION;
@@ -66,6 +76,16 @@ function UpdatePlayerPos(){
     player.y -= player.speed * Math.cos(player.angle);
 }
 
+function InBounds(alpha){
+
+    if(alpha.x < -OUT_OF_BOUNDS_BUFFER || alpha.x > canvas.width + OUT_OF_BOUNDS_BUFFER ||
+        alpha.y < -OUT_OF_BOUNDS_BUFFER || alpha.y > canvas.width + OUT_OF_BOUNDS_BUFFER){
+            return false;
+        }
+    
+    return true;
+}
+
 function UpdatePhysics() {
     UpdatePlayerPos();
     UpdateProjectilePos();
@@ -74,6 +94,10 @@ function UpdatePhysics() {
 
 function UpdateProjectilePos(){
     Projectiles.forEach(function (proj) {
+        if(InBounds(proj) == false){
+            Projectiles.delete(proj);
+            return;
+        }
         proj.x += PROJ_SPEED * Math.sin(proj.angle);
         proj.y -= PROJ_SPEED * Math.cos(proj.angle);
     });
@@ -86,7 +110,7 @@ function DrawProjectiles(){
         ctx.rotate(proj.angle);
         ctx.beginPath();
         ctx.moveTo(0, 0);
-        ctx.lineTo(0, 20);
+        ctx.lineTo(0, PROJ_LENGTH);
         ctx.strokeStyle = "#FFFFFF";
         ctx.stroke();
         ctx.restore();  
@@ -104,9 +128,36 @@ function DrawAstroids(){
     });
 }
 
+function CheckProjectileCollision(ast, proj, astRadius){
+    const _proj = {
+        x: proj.x,
+        y: proj.y,
+        angle: proj.angle
+    }    
+
+    //check 3 points along the projectile
+    const distance1 = Math.sqrt(Math.pow(ast.x - _proj.x, 2) + Math.pow(ast.y - _proj.y, 2));
+    _proj.x -= PROJ_LENGTH / 2 * Math.sin(_proj.angle);
+    _proj.y += PROJ_LENGTH / 2 * Math.cos(_proj.angle);
+    const distance2 = Math.sqrt(Math.pow(ast.x - _proj.x, 2) + Math.pow(ast.y - _proj.y, 2));
+    _proj.x -= PROJ_LENGTH * Math.sin(_proj.angle);
+    _proj.y += PROJ_LENGTH * Math.cos(_proj.angle);
+    const distance3 = Math.sqrt(Math.pow(ast.x - _proj.x, 2) + Math.pow(ast.y - _proj.y, 2));
+    if(distance1 < astRadius || distance2 < astRadius || distance3 < astRadius){ 
+        return true;
+    }
+    return false;
+}
+
 //collisions are all handled by the astroid updateds
 function UpdateAstroidPos(){
     Astroids.forEach(function (ast) {
+
+        if(InBounds(ast) == false){
+            Astroids.delete(ast);
+            return;
+        }
+
         ast.x += ast.speed * Math.sin(ast.angle);
         ast.y -= ast.speed * Math.cos(ast.angle);
 
@@ -114,7 +165,7 @@ function UpdateAstroidPos(){
 
         //collision detection with projectiles
         Projectiles.forEach(function (proj) {
-            if(CheckCollision(ast, proj, ast.radius)){
+            if(CheckProjectileCollision(ast, proj, ast.radius)){
                 Projectiles.delete(proj);
                 Astroids.delete(ast);
                 hit = true;
@@ -131,7 +182,6 @@ function UpdateAstroidPos(){
             GameOver();
             //location.reload();
         }
-
     });
 }
 
@@ -184,24 +234,25 @@ function SpawnAstroid(){
     const x = Math.floor(Math.random() * canvas.width);
     const y = Math.floor(Math.random() * canvas.height);
     const radius = Math.floor((Math.random() * (MAX_ASTROID_RADIUS - MIN_ASTROID_RADIUS)) + MIN_ASTROID_RADIUS);
-    //const angle = Math.random() * Math.PI * 2;
     const angle = Math.atan2((player.y - y), (player.x - x)) + (Math.PI/2);
-    //console.log(angle);
+    const speed = Math.floor((Math.random() * (MAX_ASTROID_SPEED - MIN_ASTROID_SPEED)) + MIN_ASTROID_SPEED);
 
     //skip spawn if on top of player
-    if(CheckCollision(player, {x: x, y: y}, PLAYER_SAFE_SPAWN_RADIUS) == false){
-        CreateAstroid(x, y, angle, radius);
+    if(upPressed == true && (CheckCollision(player, {x: x, y: y}, PLAYER_SAFE_SPAWN_RADIUS) == false)){
+        CreateAstroid(x, y, angle, radius, speed);
+    }else if(upPressed == false && CheckCollision(player, {x: x, y: y}, PLAYER_SAFE_SPAWN_RADIUS / 2) == false){ //to discourage camping and spinning
+        CreateAstroid(x, y, angle, radius - RADIUS_PUNISHMENT, Math.floor((Math.random() * (MAX_ASTROID_SPEED - MIN_ASTROID_SPEED + SPEED_PUNSHIMENT)) + MIN_ASTROID_SPEED + SPEED_PUNSHIMENT));
     }else{
         SpawnAstroid(); //try again if spawned on player
         return;
     }
 }
 
-function CreateAstroid(_x, _y, _angle, _radius){
+function CreateAstroid(_x, _y, _angle, _radius, _speed){
     Astroids.add({
         x: _x,
         y: _y,
-        speed: Math.floor((Math.random() * (MAX_ASTROID_SPEED - MIN_ASTROID_SPEED)) + MIN_ASTROID_SPEED),
+        speed: _speed,
         angle: _angle,
         radius: _radius
     });
@@ -235,9 +286,13 @@ function InitEvents() {
     document.addEventListener('keydown', (event) => {
 
         switch(event.code){
+            case "KeyA":
             case "ArrowLeft": player.moveAngle = -TURN_SPEED; break;
+            case "KeyD":
             case "ArrowRight": player.moveAngle = TURN_SPEED; break;
+            case "KeyW":
             case "ArrowUp": upPressed = true; break;
+            case "KeyS":
             case "ArrowDown": downPressed = true; break;
             //case "Space": Fire(); break;
         }
@@ -252,9 +307,13 @@ function InitEvents() {
     document.addEventListener('keyup', (event) => {
 
         switch(event.code){
+            case "KeyA":
             case "ArrowLeft": player.moveAngle = 0; break;
+            case "KeyD":
             case "ArrowRight": player.moveAngle = 0; break;
+            case "KeyW":
             case "ArrowUp": upPressed = false; break;
+            case "KeyS":
             case "ArrowDown": downPressed = false; break;
         }
     });
@@ -275,13 +334,13 @@ function StartFrameUpdate(){
     FrameUpdate = setInterval(function () { 
         frameNumber++;
     
-        if(frameNumber % spawnAstroidEvery == 0){
-            SpawnAstroid();
+        // if(frameNumber % spawnAstroidEvery == 0){
+        //     SpawnAstroid();
     
-            if(spawnAstroidEvery > MIN_ASTROID_SPAWN_COOLDOWN){
-                spawnAstroidEvery--;
-            }
-        }
+        //     if(spawnAstroidEvery > MIN_ASTROID_SPAWN_COOLDOWN){
+        //         spawnAstroidEvery--;
+        //     }
+        // }
     
         UpdatePhysics();
         Draw(); 
@@ -300,6 +359,12 @@ function StartTimerUpdate() {
         //gameover
         if(timeleft <= 0){
             TimeOut();
+        }
+
+        SpawnAstroid();
+    
+        if(spawnAstroidEvery > MIN_ASTROID_SPAWN_COOLDOWN){
+            spawnAstroidEvery--;
         }
     
         document.getElementById("timeleft").innerHTML = "Time Left: " + timeleft;
