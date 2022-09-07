ed3c41175029832cd290c59a034b2123afadc44d
lukedwards99 <70722474+lukedwards99@users.noreply.github.com>
2022-09-07 15:10:01 -0500

    started adding user controls

--- maze-solver/script.js
+++ maze-solver/script.js
@@ -1,7 +1,4 @@
-// $(function (){
-
-//     alert("ready");
-// });
+"use strict";
 
 const NUM_ROWS = 10;
 const NUM_COLS = NUM_ROWS; // future proofing
@@ -20,9 +17,30 @@ for(let i = 0; i < NUM_ROWS; i++){
     const row = $('<div></div>').addClass("row");
     $grid.append(row);
     for(let j = 0; j < NUM_COLS; j++){
-        const cell = $('<div></div>').addClass("cell").width(CELL_WIDTH).height(CELL_HEIGHT);
+        const cell = $('<div></div>').addClass("cell").width(CELL_WIDTH).height(CELL_HEIGHT).attr("data-row", i).attr("data-col", j);
         row.append(cell);
     }
 }
 
-alert("done");
+$(".cell").on('mouseenter', function() {
+    let $this = $(this);
+
+    const controls = $("<div></div>").addClass('controls');
+    controls.append($("<div></div>").height($this .height() * .2).width($this .width() * .2).css('background-color', "#000000").css("border-radius", "50%").addClass("controlsN"));
+    $this.append(controls);
+
+});
+
+$(".cell").on('mouseleave', function() {
+
+    $(this).children('.controls').remove();
+
+});
+
+$(".cell").on('click', function(){
+
+    alert($(this).attr("data-row").toString() + " " + $(this).attr("data-col").toString());
+
+});
+
+//alert("done");
